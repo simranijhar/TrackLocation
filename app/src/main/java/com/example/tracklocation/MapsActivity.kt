315e5edd7ec5ec40ae29911a424d42f3108d529f
Simran Nijhar <simran.nijhar@gmail.com>
2020-01-09 05:24:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tracklocation/MapsActivity.kt
@@ -0,0 +1,185 @@
+package com.example.tracklocation
+
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.*
+
+import com.google.android.gms.maps.model.*
+import java.util.jar.Manifest
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+
+    private lateinit var mMap: GoogleMap
+
+    private var latitude : Double = 0.toDouble()
+    private var longitude : Double = 0.toDouble()
+
+    private lateinit var lastLocation : Location
+    private var mMarker : Marker?=null
+
+    //location
+    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
+
+    private lateinit var locationRequest : LocationRequest
+
+    private lateinit var locationCallback: LocationCallback
+
+    companion object{
+        private const val MY_PERMISSION_CODE: Int = 1000
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        //Request runtime permission
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkLocationPermission()){
+                buildLocationRequest()
+                buildLocationCallBack()
+
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+            }
+        }
+        else {
+            buildLocationRequest()
+            buildLocationCallBack()
+
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.myLooper()
+            )
+        }
+
+    }
+
+    override fun onLocationChanged(location: Location?) {
+
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+    }
+
+    private fun checkLocationPermission():Boolean{
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_CODE)
+            }
+            else{
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), MY_PERMISSION_CODE)
+            }
+            return false
+        }
+        return true
+    }
+
+    private fun buildLocationRequest(){
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 10f
+    }
+
+    private fun buildLocationCallBack(){
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                lastLocation = p0!!.locations.get(p0.locations.size-1) //get last location
+
+                if(mMarker != null){
+                    mMarker!!.remove()
+                }
+
+                latitude = lastLocation.latitude
+                longitude = lastLocation.longitude
+
+                val latLng = LatLng(latitude, longitude)
+
+                val markerOptions = MarkerOptions().position(latLng).title("Your Position")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                mMarker = mMap.addMarker(markerOptions)
+
+                //move camera
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                mMap.animateCamera(CameraUpdateFactory.zoomBy(11f))
+            }
+
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                        permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        when(requestCode){
+            MY_PERMISSION_CODE->{
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        if(checkLocationPermission()){
+                            mMap.isMyLocationEnabled = true
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onStop(){
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        super.onStop()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+
+        //Init Google Play Services
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
+                mMap.isMyLocationEnabled = true
+            }
+        }
+        else{
+            mMap.isMyLocationEnabled = true
+
+        }
+
+        //enable zoom control
+        mMap.uiSettings.isZoomControlsEnabled = true
+    }
+
+}
